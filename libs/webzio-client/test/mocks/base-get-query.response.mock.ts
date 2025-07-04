0ed4e7d0d51f6994247360d5cbf51b5bbac635df
Mostafa Abdelsalam <abdelslam.mostafa@gmail.com>
2025-07-04 21:38:45 +0300

    fix: remove secrets from the code base

--- libs/webzio-client/test/mocks/base-get-query.response.mock.ts
+++ libs/webzio-client/test/mocks/base-get-query.response.mock.ts
@@ -24,7 +24,7 @@ export function baseGetQueryResponseMock(
     totalResults,
     // currently this is what returns from the api
     moreResultsAvailable: totalResults - postsSize,
-    next: '/newsApiLite?token=ace462ee-f299-444c-974e-689c7e8cdeff&ts=1751379763514&q=title%3AWeb+Development&sort=relevancy&order=desc&from=10&ns=22.511053&ni=4a75ba02fabea63a0f82e3ecc338f4bce54053c8&highlight=true&size=10',
+    next: '/newsApiLite?token=any-token-here&ts=1751379763514&q=title%3AWeb+Development&sort=relevancy&order=desc&from=10&ns=22.511053&ni=4a75ba02fabea63a0f82e3ecc338f4bce54053c8&highlight=true&size=10',
     requestsLeft: 943,
   };
 }
